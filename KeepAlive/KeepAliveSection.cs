95de5f1e7a41c869137ab6af760868ee32846e0d
Sam Winter <sam.winter@silvertech.com>
2014-03-13 16:38:51 -0400

    First commit.

---
+++ KeepAlive/KeepAliveSection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeepAlive
+{
+    public class KeepAliveSection : ConfigurationSection
+    {
+        [ConfigurationProperty("interval", DefaultValue = "1800", IsRequired = true)]
+        public int Interval
+        {
+            get { return (int)this["interval"]; }
+            set { this["interval"] = value; }
+        }
+
+        [ConfigurationProperty("site", IsRequired = false)]
+        public SiteElement Site
+        {
+            get { return (SiteElement)this["site"]; }
+            set { this["site"] = value; }
+        }
+    }
+
+    public class SiteElement : ConfigurationElement
+    {
+        [ConfigurationProperty("active", DefaultValue = "false", IsRequired = true)]
+        public bool Active
+        {
+            get { return (bool)this["active"]; }
+            set { this["active"] = value; }
+        }
+
+        [ConfigurationProperty("name", DefaultValue = "", IsRequired = true)]
+        public string Name { get; set; }
+
+        [ConfigurationProperty("url", DefaultValue = "", IsRequired = true)]
+        public string Url { get; set; }
+    }
+}
---
+++ KeepAlive/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeepAlive
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("Hi there dude.");
+
+        }
+    }
+}
